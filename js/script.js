6bfa8393459d83758b2afef710ecfa1d1cfe540c
rafael-m-silva <rafael_mauricio@hotmail.com>
2023-04-07 12:23:48 -0300

    tratamento de erro e animação no campo vazio

--- js/script.js
+++ js/script.js
@@ -1,17 +1,31 @@
 const btn = document.querySelector("#btn")
 
-btn.addEventListener("click", ()=> {
-    const link = document.querySelector("#link").value
-    const img = document.querySelector("#qr-code")
 
-    let url = `http://api.qrserver.com/v1/create-qr-code/?data=${link}&size=250x250`
-    
-    if(link.length == 0) {
+/* gerando o qrCode, tratamento de erro, animação quando o campo está vazio */
+function qrCode() {
+    let link = document.querySelector("#link")
+    let img = document.querySelector("#qr-code")
+    let linkValue = link.value
+    let url = `http://api.qrserver.com/v1/create-qr-code/?data=${linkValue}&size=250x250`
+
+    if(linkValue.length == 0) {
         url = `https://api.qrserver.com/v1/create-qr-code/?data=HelloWorld&amp;size=250x250`
 
+        link.classList.add('campo_vazio')
         img.setAttribute('src', `${url}`)
-        alert("Digite um valor valido!")
     }else {
         img.setAttribute('src', `${url}`)
     }
+}
+
+
+/* chamando a função qrcode  */
+btn.addEventListener("click", ()=> {
+    qrCode()
 })
+
+
+/* event animationnend remove a animação/class após o termino do evento */
+link.addEventListener('animationend', () => {
+    link.classList.remove('campo_vazio');
+});
