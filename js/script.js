5ac2856b9e53d05d4663ae6c83d74c5918ecd9e0
Rafael Mauricio <rafael_mauricio@hotmail.com>
2023-04-10 17:29:30 -0300

    botão de download adicionado

--- js/script.js
+++ js/script.js
@@ -1,10 +1,24 @@
 const btn = document.querySelector("#btn")
+const menu = document.querySelector(".fa-solid")
+const navbar = document.querySelector(".navbar ul")
 
+/* Menu navbar (mobile) */
+menu.addEventListener("click", ()=> {
+
+    navbar.classList.toggle("active")
+    menu.classList.toggle('fa-x')
+})
+
+window.onscroll = () => {
+    menu.classList.remove('fa-x')
+    navbar.classList.remove('active')
+  }
 
 /* gerando o qrCode, tratamento de erro, animação quando o campo está vazio */
 function qrCode() {
-    let link = document.querySelector("#link")
+    let download = document.querySelector(".btn-download")
     let img = document.querySelector("#qr-code")
+    let link = document.querySelector("#link")
     let linkValue = link.value
     let url = `http://api.qrserver.com/v1/create-qr-code/?data=${linkValue}&size=250x250`
 
@@ -13,15 +27,26 @@ function qrCode() {
 
         link.classList.add('campo_vazio')
         img.setAttribute('src', `${url}`)
+        download.classList.remove('download-active')
     }else {
         img.setAttribute('src', `${url}`)
+        download.classList.add('download-active')
     }
+}
+/* gerando o download do qrCode */
+function downloadImage() {
+    let link = document.querySelector("#link")
+    let linkValue = link.value
+    let url = `http://api.qrserver.com/v1/create-qr-code/?data=${linkValue}&size=250x250`
+    let download = document.querySelector(".btn-download #qr")
+    download.setAttribute('href', `${url}`)
 }
 
 
 /* chamando a função qrcode  */
 btn.addEventListener("click", ()=> {
     qrCode()
+    downloadImage()
 })
 
 
